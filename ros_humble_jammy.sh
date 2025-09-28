59e12386e79b74b3acd818abea22efb59e0c0bc7
Krishaay <krishaayjois@gmail.com>
2025-09-28 21:48:34 +0530

    Update Installation
    https://docs.ros.org/en/humble/Installation/Ubuntu-Install-Debs.html

--- ros_humble_jammy.sh
+++ ros_humble_jammy.sh
@@ -21,11 +21,12 @@ locale  # verify settings
 sudo apt install software-properties-common
 sudo add-apt-repository universe
 
-# GPG Key
+# ROS 2 Repositories
 sudo apt update && sudo apt install curl -y
-sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
-# Add to sources list
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+export ROS_APT_SOURCE_VERSION=$(curl -s https://api.github.com/repos/ros-infrastructure/ros-apt-source/releases/latest | grep -F "tag_name" | awk -F\" '{print $4}')
+curl -L -o /tmp/ros2-apt-source.deb "https://github.com/ros-infrastructure/ros-apt-source/releases/download/${ROS_APT_SOURCE_VERSION}/ros2-apt-source_${ROS_APT_SOURCE_VERSION}.$(. /etc/os-release && echo ${UBUNTU_CODENAME:-${VERSION_CODENAME}})_all.deb"
+sudo dpkg -i /tmp/ros2-apt-source.deb
+
 
 # Update & Upgrade
 sudo apt update && sudo apt upgrade -y
