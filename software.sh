305e2ff88c2f5387fc3034229ab93ff80c41135a
Krishaay <krishaayjois@gmail.com>
2025-09-28 21:59:10 +0530

    Add installation commands for development tools and utilities

---
+++ software.sh
@@ -0,0 +1,22 @@
+# Dev Tools
+
+## VSCODE
+sudo snap install code --classic
+
+# Music
+
+## SPOTIFY
+sudo snap install spotify
+
+# Utilities
+
+## LOCALSEND
+sudo snap install localsend
+
+## BASHTOP
+sudo apt-get install bashtop
+
+## OBS STUDIO
+sudo add-apt-repository ppa:obsproject/obs-studio
+sudo apt update
+sudo apt install obs-studio
