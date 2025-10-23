487675e81e182bd285935b110284219b0c04f599
Krishaay <krishaayjois@gmail.com>
2025-10-23 11:25:07 +0530

    Add Miniconda

--- software.sh
+++ software.sh
@@ -1,4 +1,8 @@
+cd ~
 # Dev Tools
+## Miniconda
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash ~/Miniconda3-latest-Linux-x86_64.sh
 
 ## VSCODE
 sudo snap install code --classic
@@ -20,3 +24,4 @@ sudo apt-get install bashtop
 sudo add-apt-repository ppa:obsproject/obs-studio
 sudo apt update
 sudo apt install obs-studio
+
